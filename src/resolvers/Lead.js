1a8cccf562793d359e5b74c17e36ab616f9bed1f
steinhakasevs <stein.hakase.vs@gmail.com>
2020-02-25 14:56:16 -0600

    modified leads resolver to handle database save

--- src/resolvers/Lead.js
+++ src/resolvers/Lead.js
@@ -1,11 +1,17 @@
 import { ApolloError } from 'apollo-server-express'
 import crawler from '../utils/crawler'
+import leadModel from '../schema/lead'
 
 export default {
   Query: {
     findLeads: async (root, { service, lastName, zipCode, state, city, pagesToCrawl = 10 }, context, info) => {
       try {
-        return crawler(service, lastName, zipCode, state, city, pagesToCrawl)
+        const leads = await crawler(service, lastName, zipCode, state, city, pagesToCrawl)
+        console.log(leads)
+        await leadModel.insertMany(leads, { ordered: false, rawResult: true })
+
+        const count = await leadModel.estimatedDocumentCount()
+        return count
       } catch (e) {
         throw new ApolloError(e)
       }
