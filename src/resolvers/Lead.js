1f26377b81154c7f080be321534559a3f7957258
victor stein <stein.hakase.vs@gmail.com>
2020-03-11 12:35:09 -0600

    formating and date filter with moment to document count

--- src/resolvers/Lead.js
+++ src/resolvers/Lead.js
@@ -1,6 +1,7 @@
 import { ApolloError } from 'apollo-server-express'
 import crawler from '../utils/crawler'
 import leadModel from '../schema/lead'
+import moment from 'moment'
 
 export default {
   Query: {
@@ -8,26 +9,24 @@ export default {
       try {
         const leads = await crawler(service, lastName, zipCode, state, city, pagesToCrawl)
 
-		console.log(leads)
-
         if (leads.length) {
-		    for (const lead of leads) {
-				try {
-					await leadModel.create({
-					  ...lead,
-					  keyword: `${service} ${lastName} ${zipCode} ${state} ${city}`,
-					  crawledBy
-					})
-				} catch(e) {
-					console.log('found a duplicate')
-				}
-			}
-		}
+          for (const lead of leads) {
+            try {
+              await leadModel.create({
+                ...lead,
+                keyword: `${service} ${lastName} ${zipCode} ${state} ${city}`,
+                crawledBy
+              })
+            } catch (e) {
+              console.log('found a duplicate')
+            }
+          }
+        }
 
-        const count = await leadModel.countDocuments({ createdAt: { $gte: new Date() } })
+        const count = await leadModel.countDocuments({ createdAt: { $gte: moment().startOf('day').toDate() } })
         return count
       } catch (e) {
-		console.log(e)
+        console.log(e)
         throw new ApolloError(e)
       }
     }
