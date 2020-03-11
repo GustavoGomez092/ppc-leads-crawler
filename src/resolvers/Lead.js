197f4506f3d4b0b7a33be13a0a9e272cdb6189ce
steinhakasevs <stein.hakase.vs@gmail.com>
2020-03-11 10:13:55 -0600

    added keyword and crawledBy to the model data

--- src/resolvers/Lead.js
+++ src/resolvers/Lead.js
@@ -4,12 +4,16 @@ import leadModel from '../schema/lead'
 
 export default {
   Query: {
-    findLeads: async (root, { service, lastName, zipCode, state, city, pagesToCrawl = 10 }, context, info) => {
+    findLeads: async (root, { crawledBy = '', service, lastName, zipCode, state, city, pagesToCrawl = 10 }, context, info) => {
       try {
         const leads = await crawler(service, lastName, zipCode, state, city, pagesToCrawl)
 
         for (const lead of leads) {
-          await leadModel.create(lead)
+          await leadModel.create({
+            ...lead,
+            keyword: `${service} ${lastName} ${zipCode} ${state} ${city}`,
+            crawledBy
+          })
         }
 
         const count = await leadModel.estimatedDocumentCount()
