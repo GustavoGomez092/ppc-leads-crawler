71ee6f4b43582cad2aac0660cdd202124cf08358
steinhakasevs <stein.hakase.vs@gmail.com>
2020-08-28 15:07:02 -0600

    added logic to check duplicate records

--- src/resolvers/Lead.js
+++ src/resolvers/Lead.js
@@ -7,20 +7,35 @@ export default {
   Query: {
     findLeads: async (root, { crawledBy = '', service = '', lastName = '', zipCode = '', state = '', city = '', pagesToCrawl = 10 }, context, info) => {
       try {
-        const leads = await crawler(service, lastName, zipCode, state, city, pagesToCrawl)
+        // get leads
+        const leads = await crawler.getLeads(service, lastName, zipCode, state, city, pagesToCrawl)
 
-        if (leads.length) {
-          for (const lead of leads) {
-            try {
-              await leadModel.create({
-                ...lead,
-                keyword: `${service}`,
-                crawledBy,
-                state: `${state}`,
-                city: `${city}`
-              })
-            } catch (e) {
-              console.log('found a duplicate')
+        // Get the urls of the leads only
+        const urls = leads.map(u => u.adLink)
+
+        // Check leads agains DB
+        const newLeads = await leadModel.find({ adLink: urls }, { adLink: 1 })
+
+        // Compare the existing vs the crawled
+        const curatedLeads = leads.filter(u => !newLeads.some(x => x.adLink === u.adLink))
+
+        if (curatedLeads.length) {
+          // Crawl the pages
+          const crawledPages = await crawler.crawlResults(curatedLeads)
+
+          if (crawledPages.length) {
+            for (const lead of crawledPages) {
+              try {
+                await leadModel.create({
+                  ...lead,
+                  keyword: `${service}`,
+                  crawledBy,
+                  state: `${state}`,
+                  city: `${city}`
+                })
+              } catch (e) {
+                console.log('found a duplicate')
+              }
             }
           }
         }
